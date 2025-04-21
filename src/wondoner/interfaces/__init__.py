0e7bf7335591bc8303b8eca4f80beabebc20e76c
Tom Boone <tbc@tomboone.com>
2025-04-20 21:22:23 -0400

    update version

--- src/wondoner/interfaces/__init__.py
+++ src/wondoner/interfaces/__init__.py
@@ -4,7 +4,7 @@ from .interfaces import TaskSourceIntegration
 from .models import StandardTask
 from .enums import TaskStatus
 
-__version__ = "0.1.1"  # Version of the interfaces module
+__version__ = "0.1.2"  # Version of the interfaces module
 
 # Controls what 'from task_aggregator_interfaces import *' imports
 __all__ = [
